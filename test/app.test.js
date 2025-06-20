b097cf3c838fd435376b1bfa6c822b28137c5ef8
hlcxpl <hlcxpl@gmai.com>
2025-06-19 22:14:51 -0400

    update 6

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,15 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('/src/app');
+
+
+
+
 describe('GET /api/inmuebles', () => {
- it('debería retornar una lista de inmuebles', async () => {
- const res = await request(app).get('/api/inmuebles');
- expect(res.statusCode).toBe(200);
- expect(res.body.length).toBeGreaterThan(0);
- expect(res.body[0]).toHaveProperty('direccion');
- expect(res.body[0]).toHaveProperty('precio');
- });
+    it('debería retornar una lista de inmuebles', async () => {
+        const res = await request(app).get('/api/inmuebles');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBeGreaterThan(0);
+        expect(res.body[0]).toHaveProperty('direccion');
+        expect(res.body[0]).toHaveProperty('precio');
+    });
 });
